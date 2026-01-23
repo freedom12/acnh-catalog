85c102181f5138b0ac5d8c3ff773a92b15bd69e1
wanghuai <wanghuai@pwrd.com>
2026-01-23 09:55:42 +0800

    构建自动排出不需要的目录

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/acnh-catalog/',
+  publicDir: false,
   build: {
     target: 'es2015',
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        { src: 'public/catalog_items.json', dest: '.' },
+        { src: 'public/translations.json', dest: '.' },
+        { src: 'public/config/**', dest: 'config' },
+        { src: 'public/img/hhp_island_icon/**', dest: 'img/hhp_island_icon' },
+        { src: 'public/img/hhp_island_img/**', dest: 'img/hhp_island_img' },
+        { src: 'public/img/icon/**', dest: 'img/icon' },
+        { src: 'public/img/pattern/**', dest: 'img/pattern' },
+        { src: 'public/img/polishing/**', dest: 'img/polishing' },
+        { src: 'public/img/soundscape/**', dest: 'img/soundscape' },
+        // { src: 'public/sound/**', dest: 'sound' },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
